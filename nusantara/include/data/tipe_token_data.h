88c55f6f65d0717be8fd3c51405504a950568f69
Fern-Aerell <fernaerell2020@gmail.com>
2024-04-01 18:38:36 +0700

    Menambahkan data tipe token

---
+++ nusantara/include/data/tipe_token_data.h
@@ -0,0 +1,112 @@
+#pragma once
+
+#define TP_KEMBALI_KE_AWAL_KARAKTER "[\r]"
+#define TT_KEMBALI_KE_AWAL_KARAKTER "kembali ke awal karakter"
+
+#define TP_BARIS_BARU "[\n]"
+#define TT_BARIS_BARU "baris baru"
+
+#define TP_TAB "[\t]"
+#define TT_TAB "tab"
+
+#define TP_SPASI "[ ]"
+#define TT_SPASI "spasi"
+
+#define TP_KOMENTAR_SATU_BARIS "//[^\n]*"
+#define TT_KOMENTAR_SATU_BARIS "komentar satu baris"
+
+#define TP_KOMENTAR_BANYAK_BARIS "/\\*[^(/*)]*\\*/"
+#define TT_KOMENTAR_BANYAK_BARIS "komentar banyak baris"
+
+#define TP_MUAT "muat"
+#define TT_MUAT "muat"
+
+#define TP_LUAR "luar"
+#define TT_LUAR "luar"
+
+#define TP_BILANGAN "bilangan"
+#define TT_BILANGAN "bilangan"
+
+#define TP_TEKS "teks"
+#define TT_TEKS "teks"
+
+#define TP_DINAMIS "dinamis"
+#define TT_DINAMIS "dinamis"
+
+#define TP_IDENTIFIKASI "[a-zA-Z_][a-zA-Z0-9_]*"
+#define TT_IDENTIFIKASI "identifikasi"
+
+#define TP_ANGKA "0-9"
+#define TT_ANGKA "angka"
+
+#define TP_TAMBAH "[+]"
+#define TT_TAMBAH "tambah"
+
+#define TP_TANDA_HUBUNG "-"
+#define TT_TANDA_HUBUNG "tanda hubung"
+
+#define TP_ASTERIS "[*]"
+#define TT_ASTERIS "asteris"
+
+#define TP_GARIS_MIRING "/"
+#define TT_GARIS_MIRING "garis miring"
+
+#define TP_PERSEN "[%]"
+#define TT_PERSEN "persen"
+
+#define TP_SAMA_DENGAN "="
+#define TT_SAMA_DENGAN "sama dengan"
+
+#define TP_SERU "!"
+#define TT_SERU "seru"
+
+#define TP_DOLAR "[$]"
+#define TT_DOLAR "dolar"
+
+#define TP_GARIS_BAWAH "_"
+#define TT_GARIS_BAWAH "garis bawah"
+
+#define TP_KOMA ","
+#define TT_KOMA "koma"
+
+#define TP_TITIK "\\."
+#define TT_TITIK "titik"
+
+#define TP_TITIK_DUA ":"
+#define TT_TITIK_DUA "titik dua"
+
+#define TP_TITIK_KOMA ";"
+#define TT_TITIK_KOMA "titik koma"
+
+#define TP_KUTIP_SATU "'"
+#define TT_KUTIP_SATU "kutip satu"
+
+#define TP_GARIS_VERTIKAL R"(\|)"
+#define TT_GARIS_VERTIKAL "garis vertikal"
+
+#define TP_KURUNG_BUKA R"(\()"
+#define TT_KURUNG_BUKA "kurung buka"
+
+#define TP_KURUNG_TUTUP R"(\))"
+#define TT_KURUNG_TUTUP "kurung tutup"
+
+#define TP_KURUNG_SIKU_BUKA R"(\[)"
+#define TT_KURUNG_SIKU_BUKA "kurung siku buka"
+
+#define TP_KURUNG_SIKU_TUTUP R"(\])"
+#define TT_KURUNG_SIKU_TUTUP "kurung siku tutup"
+
+#define TP_KURUNG_KURAWAL_BUKA R"(\{)"
+#define TT_KURUNG_KURAWAL_BUKA "kurung kurawal buka"
+
+#define TP_KURUNG_KURAWAL_TUTUP R"(\})"
+#define TT_KURUNG_KURAWAL_TUTUP "kurung kurawal tutup"
+
+#define TP_KARAKTER "."
+#define TT_KARAKTER "karakter"
+
+#define TP_AKHIR_DARI_FILE "\\0"
+#define TT_AKHIR_DARI_FILE "akhir dari file"
+
+#define TP_TIDAK_DIKETAHUI ""
+#define TT_TIDAK_DIKETAHUI "tidak diketahui"
